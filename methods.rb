8a37e724d477119411254c5b3cceea68998b6500
antonyosebe <osebe.anto@gmail.com>
2023-02-06 22:40:19 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name="Antony")
+    puts "Hello, #{name}!"
+end
+greet
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+def add(num1,num2)
+    return num1 + num2
+end
+sum=add(4,5)
+
+def halve (num)
+    if num.class!=Integer
+     return nil
+    end
+    num/2
+end
+halve(10)
